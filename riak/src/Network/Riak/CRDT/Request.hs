e4661af38dfaec9402b32b89d54d73d55eda20c1
Yu-ri Gordon <yuri.cho@gmail.com>
2022-04-05 19:45:47 -0400

    escape missing from CRDT request module

--- riak/src/Network/Riak/CRDT/Request.hs
+++ riak/src/Network/Riak/CRDT/Request.hs
@@ -6,45 +6,57 @@
 -- Maintainer:  Tim McGilchrist <timmcgil@gmail.com>, Mark Hibberd <mark@hibberd.id.au>
 -- Stability:   experimental
 -- Portability: portable
---
-module Network.Riak.CRDT.Request (
-    get
-  , counterUpdate
-  , setUpdate
-  , mapUpdate
-  ) where
+module Network.Riak.CRDT.Request
+  ( get,
+    counterUpdate,
+    setUpdate,
+    mapUpdate,
+  )
+where
 
-import           Data.ByteString (ByteString)
+import Data.ByteString (ByteString)
 import qualified Data.Riak.Proto as Proto
-import           Network.Riak.CRDT.Ops
+import Network.Riak.CRDT.Ops
 import qualified Network.Riak.CRDT.Types as CRDT
-import           Network.Riak.Lens
-import           Network.Riak.Types
+import Network.Riak.Escape (escape)
+import Network.Riak.Lens
+import Network.Riak.Types
 
-counterUpdate :: [CRDT.CounterOp]
-              -> BucketType -> Bucket -> Key
-              -> Proto.DtUpdateReq
+counterUpdate ::
+  [CRDT.CounterOp] ->
+  BucketType ->
+  Bucket ->
+  Key ->
+  Proto.DtUpdateReq
 counterUpdate ops = update (counterUpdateOp ops)
 
-setUpdate :: [CRDT.SetOp]
-          -> BucketType -> Bucket -> Key
-          -> Proto.DtUpdateReq
+setUpdate ::
+  [CRDT.SetOp] ->
+  BucketType ->
+  Bucket ->
+  Key ->
+  Proto.DtUpdateReq
 setUpdate ops = update (setUpdateOp ops)
 
-mapUpdate :: [CRDT.MapOp]
-          -> BucketType -> Bucket -> Key
-          -> Proto.DtUpdateReq
+mapUpdate ::
+  [CRDT.MapOp] ->
+  BucketType ->
+  Bucket ->
+  Key ->
+  Proto.DtUpdateReq
 mapUpdate ops = update (mapUpdateOp ops)
 
 update :: Proto.DtOp -> BucketType -> Bucket -> Key -> Proto.DtUpdateReq
-update op t b k = Proto.defMessage
-                    & Proto.bucket .~ b
-                    & Proto.key .~ k
-                    & Proto.op .~ op
-                    & Proto.type' .~ t
+update op t b k =
+  Proto.defMessage
+    & Proto.bucket .~ escape b
+    & Proto.key .~ escape k
+    & Proto.op .~ op
+    & Proto.type' .~ t
 
 get :: ByteString -> ByteString -> ByteString -> Proto.DtFetchReq
-get t b k = Proto.defMessage
-              & Proto.bucket .~ b
-              & Proto.key .~ k
-              & Proto.type' .~ t
+get t b k =
+  Proto.defMessage
+    & Proto.bucket .~ escape b
+    & Proto.key .~ escape k
+    & Proto.type' .~ t
